0eb18101d16c9c539075df07059c6fbe9a4fd3ca
kribac12 <89196018+kribac12@users.noreply.github.com>
2023-11-01 12:14:15 +0100

    Unit-test for login function passed, login function fetches and stores a token in browser storage

--- src/js/tests/login.test.js
+++ src/js/tests/login.test.js
@@ -1,19 +1,39 @@
 import { login } from "../api/auth/login";
-import { save } from "../storage";
+import * as storage from "../storage/save.js";
+
+//Mocking localStorage
+
+global.localStorage = {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
 
 global.fetch = jest.fn();
 
 describe("login function", () => {
+  beforeEach(() => {
+    localStorage.setItem.mockClear();
+    localStorage.getItem.mockClear();
+    localStorage.removeItem.mockClear();
+    localStorage.clear.mockClear();
+    fetch.mockClear();
+  });
+
   it("should store token in browser storage if login success", async () => {
     fetch.mockResolvedValueOnce({
       ok: true,
       json: async () => ({ accessToken: "test-token", name: "Doggy Doggo" }),
     });
 
-    const saveMock = jest.fn();
-    save.save = saveMock;
+    // Mock save function
+    jest.spyOn(storage, "save").mockImplementation((key, value) => {
+      localStorage.setItem(key, value);
+    });
+
     await login("doggo@example.com", "password");
 
-    expect(saveMock).toHaveBeenCalledWith("token", "test-token");
+    expect(localStorage.setItem).toHaveBeenCalledWith("token", "test-token");
   });
 });
